85dafa662b31f69f34bd47577591e07be0038e7c
Sylwia Biniek <sylwiaxbiniek@gmail.com>
2020-08-21 22:39:17 +0200

    zmiana wyglądu interfejsu

--- src/components/Formularz.vue
+++ src/components/Formularz.vue
@@ -1,134 +1,144 @@
 <template>
-<v-container>
-  <v-form @submit.prevent="submit">
-    <v-card-text>
-      <!-- IMIE -->
-      <v-text-field
-        v-model="imie"
-        :error-messages="imieErrors"
-        label="Imię"
-        required
-        @input="$v.imie.$touch()"
-        @blur="$v.imie.$touch()"
-      ></v-text-field>
-
-      <!-- NAZWISKO -->
-      <v-text-field
-        v-model="nazwisko"
-        :error-messages="nazwiskoErrors"
-        label="Nazwisko"
-        required
-        @input="$v.nazwisko.$touch()"
-        @blur="$v.nazwisko.$touch()"
-      ></v-text-field>
-
-      <!-- PESEL -->
-      <v-text-field
-        v-model="pesel"
-        :error-messages="peselErrors"
-        label="PESEL"
-        required
-        @input="$v.pesel.$touch()"
-        @blur="$v.pesel.$touch()"
-      ></v-text-field>
-
-      <!-- ODDZIAL -->
-      <v-select
-        v-model="oddzial"
-        :items="oddzialy"
-        :error-messages="oddzialErrors"
-        label="Oddział"
-        required
-        @change="$v.oddzial.$touch()"
-        @blur="$v.oddzial.$touch()"
-      ></v-select>
-
-      <!-- NAZWA LEKU -->
-      <v-select
-        v-model="lek"
-        :items="leki"
-        :error-messages="lekErrors"
-        label="Nazwa leku"
-        required
-        @change="$v.lek.$touch()"
-        @blur="$v.lek.$touch()"
-      ></v-select>
-
-      <!-- ILOSC LEKU -->
-      <v-text-field
-        v-model="ilosc"
-        :error-messages="iloscErrors"
-        label="Ilość"
-        required
-        @input="$v.ilosc.$touch()"
-        @blur="$v.ilosc.$touch()"
-      ></v-text-field>
-
-      <!-- DATA -->
-      <v-menu
-        ref="menu_data"
-        v-model="menu_data"
-        :close-on-content-click="false"
-        :return-value.sync="data"
-        transition="scale-transition"
-        offset-y
-        min-width="290px"
-      >
-        <template v-slot:activator="{ on, attrs }">
+  <v-container>
+    <v-form @submit.prevent="submit">
+      <v-subheader>Dane pacjenta</v-subheader>
+      <v-card-subtitle style="max-width: 1000px" flat class="ma-16 text-xs-center">
+        <v-card-text>
+          <!-- IMIE -->
           <v-text-field
-            v-model="data"
-            :error-messages="dataErrors"
-            label="Data podania"
-            prepend-icon="event"
-            readonly
-            v-bind="attrs"
-            v-on="on"
+            v-model="imie"
+            :error-messages="imieErrors"
+            label="Imię"
+            required
+            @input="$v.imie.$touch()"
+            @blur="$v.imie.$touch()"
           ></v-text-field>
-        </template>
-        <v-date-picker v-model="data" no-title scrollable>
-          <v-spacer></v-spacer>
-          <v-btn text color="primary" @click="menu_data = false">Cancel</v-btn>
-          <v-btn text color="primary" @click="$refs.menu_data.save(data)">OK</v-btn>
-        </v-date-picker>
-      </v-menu>
 
-      <!-- CZAS -->
-      <v-menu
-        ref="menu_czas"
-        v-model="menu_czas"
-        :close-on-content-click="false"
-        :nudge-right="40"
-        :return-value.sync="czas"
-        transition="scale-transition"
-        offset-y
-        max-width="290px"
-        min-width="290px"
-      >
-        <template v-slot:activator="{ on, attrs }">
+          <!-- NAZWISKO -->
           <v-text-field
-            v-model="czas"
-            :error-messages="czasErrors"
-            label="Pora podania"
-            prepend-icon="access_time"
-            readonly
-            v-bind="attrs"
-            v-on="on"
+            v-model="nazwisko"
+            :error-messages="nazwiskoErrors"
+            label="Nazwisko"
+            required
+            @input="$v.nazwisko.$touch()"
+            @blur="$v.nazwisko.$touch()"
           ></v-text-field>
-        </template>
-        <v-time-picker
-          v-if="menu_czas"
-          v-model="czas"
-          full-width
-          @click:minute="$refs.menu_czas.save(czas)"
-        ></v-time-picker>
-      </v-menu>
-    </v-card-text>
-    <v-card-actions>
-      <v-btn class="mr-4" @click="submit">submit</v-btn>
-      <v-btn @click="clear">clear</v-btn>
-    </v-card-actions>
-  </v-form>
-</v-container>
+
+          <!-- PESEL -->
+          <v-text-field
+            v-model="pesel"
+            :error-messages="peselErrors"
+            label="PESEL"
+            required
+            @input="$v.pesel.$touch()"
+            @blur="$v.pesel.$touch()"
+          ></v-text-field>
+
+          <!-- ODDZIAL -->
+          <v-select
+            v-model="oddzial"
+            :items="oddzialy"
+            :error-messages="oddzialErrors"
+            label="Oddział"
+            required
+            @change="$v.oddzial.$touch()"
+            @blur="$v.oddzial.$touch()"
+          ></v-select>
+        </v-card-text>
+      </v-card-subtitle>
+
+      <v-divider :inset="inset"></v-divider>
+      <v-subheader>Informacje o leku</v-subheader>
+      <v-card-subtitle style="max-width: 1000px" flat class="ma-16 text-xs-center">
+        <v-card-text>
+          <!-- NAZWA LEKU -->
+          <v-select
+            v-model="lek"
+            :items="leki"
+            :error-messages="lekErrors"
+            label="Nazwa leku"
+            required
+            @change="$v.lek.$touch()"
+            @blur="$v.lek.$touch()"
+          ></v-select>
+
+          <!-- ILOSC LEKU -->
+          <v-text-field
+            v-model="ilosc"
+            :error-messages="iloscErrors"
+            label="Ilość"
+            required
+            @input="$v.ilosc.$touch()"
+            @blur="$v.ilosc.$touch()"
+          ></v-text-field>
+
+          <!-- DATA -->
+          <v-menu
+            ref="menu_data"
+            v-model="menu_data"
+            :close-on-content-click="false"
+            :return-value.sync="data"
+            transition="scale-transition"
+            offset-y
+            min-width="290px"
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-text-field
+                v-model="data"
+                :error-messages="dataErrors"
+                label="Data podania"
+                prepend-icon="event"
+                readonly
+                v-bind="attrs"
+                v-on="on"
+              ></v-text-field>
+            </template>
+            <v-date-picker v-model="data" no-title scrollable>
+              <v-spacer></v-spacer>
+              <v-btn depressed color="primary" @click="menu_data = false">Cancel</v-btn>
+              <v-btn depressed color="primary"  @click="$refs.menu_data.save(data)">OK</v-btn>
+            </v-date-picker>
+          </v-menu>
+
+          <!-- CZAS -->
+          <v-menu
+            ref="menu_czas"
+            v-model="menu_czas"
+            :close-on-content-click="false"
+            :nudge-right="40"
+            :return-value.sync="czas"
+            transition="scale-transition"
+            offset-y
+            max-width="290px"
+            min-width="290px"
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-text-field
+                v-model="czas"
+                :error-messages="czasErrors"
+                label="Pora podania"
+                prepend-icon="access_time"
+                readonly
+                v-bind="attrs"
+                v-on="on"
+              ></v-text-field>
+            </template>
+            <v-time-picker
+              v-if="menu_czas"
+              v-model="czas"
+              full-width
+              @click:minute="$refs.menu_czas.save(czas)"
+            ></v-time-picker>
+          </v-menu>
+        </v-card-text>
+      </v-card-subtitle>
+
+      <v-card-actions>
+        <v-btn depressed large color="primary" class="mr-4" @click="submit">wyślij</v-btn>
+        <v-btn depressed large color="primary" @click="clear">wyczyść</v-btn>
+      </v-card-actions>
+    </v-form>
+  </v-container>
 </template>
 
 
@@ -232,7 +242,6 @@ export default {
       this.$v.$touch();
 
       if (!this.$v.$invalid) {
-        
         let zlecenie = {
           imie: this.imie,
           nazwisko: this.nazwisko,
@@ -242,9 +251,8 @@ export default {
           ilosc: this.ilosc,
           data: this.data,
           czas: this.czas
-        }
-        this.$emit('noweZlecenie', zlecenie);
-        
+        };
+        this.$emit("noweZlecenie", zlecenie);
       }
     },
     clear() {
